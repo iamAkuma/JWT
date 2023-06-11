41aba4fa72c42396e695cf5260fb87b5718d9263
Saral Bajracharya <rmanbajra@gmail.com>
2023-06-11 18:37:31 +0545

    JWT

--- app.js
+++ app.js
@@ -9,4 +9,25 @@ app.get('/api', (req, res) => {
     });
 });
 
+app.post('/api/posts', (req, res) => {
+    res.json({
+        message: 'Post created'
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    //Mock User
+    const user = {
+        id: 1,
+        username: 'Saral',
+        email: 'saralbjr@gmail.com'
+    }
+    jwt.sign({user: user}, 'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
+const PORT = 5000;
 app.listen(5000, ()=> console.log(`Server started on ${PORT}`))
