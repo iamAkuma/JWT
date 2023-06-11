bc1b0777bbea5b33a681b2f5f339c63dbfb2df16
Saral Bajracharya <rmanbajra@gmail.com>
2023-06-11 18:51:39 +0545

    JWT

--- app.js
+++ app.js
@@ -1,33 +1,131 @@
+// const express = require('express');
+// const jwt = require('jsonwebtoken');
+
+// const app = express();
+
+// app.get('/api', (req, res) => {
+//     res.json({
+//         message: 'Welcome to the API'
+//     });
+// });
+
+// app.post('/api/posts', verifyToken, (req, res) => {
+//     jwt.verify(req.token, 'secretkey', (err, authData)=> {
+//         if(err) {
+//             res.sendStatus(403);
+//         } else {
+//             res.json({
+//                 message: 'Post created',
+//                 authData
+//             });
+//         }
+//     });
+   
+// });
+
+// app.post('/api/login', (req, res) => {
+//     //Mock User
+//     const user = {
+//         id: 1,
+//         username: 'Saral',
+//         email: 'saralbjr@gmail.com'
+//     }
+//     jwt.sign({user: user}, 'secretkey', (err, token) => {
+//         res.json({
+//             token
+//         });
+//     });
+// });
+
+// //FORMAT OF TOKEN
+// // Authoriztion: Bearer <access_token>
+
+// //Verify Token
+// function verifyToken(req, res, next) {
+//     //GET auth header value
+//     const bearerHeader = req.headers['authoriztion'];
+//     //Check if bearer is undefined
+//     if(typeof bearerHeader !== 'undefined') {
+//         //Split at the space
+//         const bearer = bearerHeader.split(' ');
+//         //Get token from array
+//         const bearerToken = bearer[1];
+//         //Set the token
+//         req.token = bearerToken;
+//         //Next middleware
+//         next();
+//     }else{
+//         //Forbidden
+//         res.sendStatus(403);
+//     }
+
+
+// }
+
+// const PORT = 5000;
+// app.listen(5000, ()=> console.log(`Server started on ${PORT}`))
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const app = express();
 
 app.get('/api', (req, res) => {
-    res.json({
-        message: 'Welcome to the API'
-    });
+  res.json({
+    message: 'Welcome to the API'
+  });
 });
 
-app.post('/api/posts', (req, res) => {
-    res.json({
-        message: 'Post created'
-    });
+app.post('/api/posts', verifyToken, (req, res) => {  
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
 });
 
 app.post('/api/login', (req, res) => {
-    //Mock User
-    const user = {
-        id: 1,
-        username: 'Saral',
-        email: 'saralbjr@gmail.com'
-    }
-    jwt.sign({user: user}, 'secretkey', (err, token) => {
-        res.json({
-            token
-        });
+  // Mock user
+  const user = {
+    id: 1, 
+    username: 'brad',
+    email: 'brad@gmail.com'
+  }
+
+  jwt.sign({user}, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+    res.json({
+      token
     });
+  });
 });
 
-const PORT = 5000;
-app.listen(5000, ()=> console.log(`Server started on ${PORT}`))
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+
+}
+
+app.listen(5000, () => console.log('Server started on port 5000'));
